cf5d35e25dd066071e4b688c3a2c9d94fc5ad9e8
JluceroB <93612514+JluceroB@users.noreply.github.com>
2023-10-03 17:16:39 -0400

    Update ExeInstruc.cpp

--- ExeInstruc.cpp
+++ ExeInstruc.cpp
@@ -2,112 +2,121 @@
 #include <fstream>
 #include <vector>
 #include <map>
+#include<unordered_map>
 using namespace std;
 
-//Execute AND instruction
-void executeAND(vector<::string>& operands){
-    //Assuming operands are in the form {"AND","xd","x1","x2"}
-    uint64_t results = getRegisterValue(operands[2])//Retrieves the value of the register specifed by the second operand("Xn")
-     & getRegisterValue(operands[3]);//Retrieves the value of the register specified by the third operand ("Xm")
-    setRegisterValue(operands[1],result);//Stores the result of the AND operation in the register specified by the first operand ("xd")
+unordered_map<string, int>register;//temp register 
+
+int memory[1000] = { 0 };// simple temp mem array
+
+//Execute AND instruction 
+void executeAND(string& op1, string& op2, string& op3) {
+    register[op1] = register[op2] & register[op3];
 }
 //Execute ADD instruction
-void executeADD(vector<::string>& operands){
-  //Assuming operands are in the form {"ADD","xd","x1","x2"}
-    uint64_t results = getRegisterValue(operands[2])//Retrieves the value of the register specifed by the second operand("Xn")
-     + getRegisterValue(operands[3]);//Retrieves the value of the register specified by the third operand ("Xm")
-    setRegisterValue(operands[1],result);//Stores the result of the AND operation in the register specified by the first operand ("xd")  
+void executeADD(string& op1, string &op2, string& op3) {
+    //Assuming operands are in the form {"ADD","xd","x1","x2"}
+    register[op1] = register[op2] + registers[op3];
 }
-//Execute B instruction
-void executeB(vector<::string>& operands){
+//Execute B instruction 
+void executeB(string& destination) {
     //Assuming operands are in the form {"B","target_address"}
-    if(operands.size()!=2){
-        cout<<"Error: Invalid number of operands for branch instruction"<<
-        return;//checks whether vector has only two elements if not it gives error message
-    uint64_t target_address = stoull(operands[1], nullptr,0);//second element in vector is taddr, is converted to unsigned ll(64-bit integer). 'nullptr' is pointer set to  f_char in string
-        }
+    cout<<"Branching to: "<<destination<<"\"
     pc_register = target_address;//update program counter to the target address
 }
-//Execute B.GET instruction
-void executeBGET(vector<::string>& operands){
+//Execute B.GET instruction (branch is greater or equal to) 
+void executeBGET(string& destination, string& op1, string& op2) {
     //Assuming operands are in the form {"B","target_address"}
-    if(operands.size()!=2){
-        cout<<"Error: Invalid number of operands for branch instruction"<<
-        return;//checks whether vector has only two elements if not it gives error message
-        }
-    pc_register = target_address;//update program counter to the target address
+    int operand1 = register[op1]; //storing value from register op1
+    int operand2 = atoi(op2); //assumes op2 is the immidiate value converts to integer
+    
+    //checks conditon which is if it is greater than
+    if (operand1 >= operand2) {
+        cout << "B.GET: Branch to: " << destination << "\n";
+    }
+    else {
+        cout << "B.GET: Cannot branch condition has not been met.\n";
+    }
 }
-//Execute B.LE instruction
-void executeBLE(vector<::string>& operands){
-    if(operands.size()!=2){
-        cout<<"Error: Invalid number of operands for branch instruction"<<
-        return;//checks whether vector has only two elements if not it gives error message
-        }
-        //Get mem address from the operands
-        uint64_t mem_address = stoull(operands[1], nullptr,0 );
+//Execute B.LE instruction(less then or equal to)
+void executeBLE(string& destination, string& op1, string& op2) {
+    //Assuming operands are in the form {"B","target_address"}
+    int operand1 = register[op1]; //storing value from register op1
+    int operand2 = atoi(op2); //assumes op2 is the immidiate value converts to integer
 
-        //check if mem address is less than or equal
-        if(mem_address >=sizeof(memory)/sizeof(memory[0])){
-            cout<<"Error: Memory address not in range"
-            return 0;
-        }
+    //checks conditon which is if it is greater than
+    if (operand1 <= operand2) {
+        cout << "B.GET: Branch to: " << destination << "\n";
+    }
+    else {
+        cout << "B.GET: Cannot branch condition has not been met.\n";
+    }
 }
-//Execute CMP instruction
-void executeCMP(vector<::string>& operands){
-    //Assuming operands are in the form {"CMP","x1","x2"}
+//Execute CMP instruction 
+void executeCMP(string& op1, string& op2) {
+    //Assuming operands are in the form {"B","target_address"}
+    int zFlag = 0;
+    int nFlag = 0;
+    int operand1 = register[op1]; //storing value from register op1
+    int operand2 = register[op2];//storing value from register op2
 
-    //Get values from registers x1 and x2
-    uint64_t x1_register = x_registers[stoi(operands[1].substr(1))];
-    uint64_t x2_register = x_registers[stoi(operands[2].substr(1))];
-    //perform comparison
-    uint64_t results = static_cast<uint64_t<(x1_register) - static_cast<uint64_t>(x2_register);
+    //perform substraction
+    int result = operand1 - operand2;
+    //set N(negative) and Z(zero) flags
+    if (result == 0) {
+        zFlag++;
+    }
+    else if (result < 0) {
+        nFlag++;
+    }
+    cout << "CMP: Zerp Flag = " << zFlag << "Negative Flag = " << nFlag << "\n";
 }
 //Execute EOR instruction
-void executeEOR(vector<::string>& operands){
-    
+void executeEOR(string& op1, string& op2, string& op3) {
+    register[op1] = register[op1] ^ register[op2];
+
 }
 //Execute LDR instruction
-void executeLDR(vector<::string>& operands){
-    
+void executeLDR(string& op1, string& op2) {
+    int address = register[op2];//storing the value stored in op2 inside of address
+    register[op1] = memory[address]; //assiging that memory address to the specified register
 }
-//Execute LDRB instruction
-void executeLDRB(vector<::string>& operands){
-    
+//Execute LDRB instruction (loads a byte from memory into a register)
+void executeLDRB(string& op1, string& baseReg, int regOffset) {
+    int address = register[baseReg] + regOffset; //Takes the base registers mem address  and adds the offset to calculate the new mem address
+    register[op1] = memory[address]; //the byte is loaded from mem to the location of destination address
 }
 //Execute MOV instruction
-void executeMOV(vector<::string>& operands){
-    
+void executeMOV(string& destination, string& op1) {
+    int operand1 = [op1];
+    register[destination] = operand1;
 }
-//Execute MUL instruction
-void executeMUL(vector<::string>& operands){
-    void executeSUB(vector<::string>& operands){
-    //Assuming operands are in the form {"ADD","xd","x1","x2"}
-    uint64_t results = getRegisterValue(operands[2])//Retrieves the value of the register specifed by the second operand("Xn")
-     * getRegisterValue(operands[3]);//Retrieves the value of the register specified by the third operand ("Xm")
-    setRegisterValue(operands[1],result);//Stores the result of the AND operation in the register specified by the first operand ("xd")  
-    
+//Execute MUL instruction 
+void executeMUL(string& op1, string& op2, string& op3) {
+
+    register[op1] = register[op2] * register[op3]
 }
-//Execute NOP instruction
-void executeNOP(vector<::string>& operands){
-    
-}
-//Execute RET instruction
-void executeRET(vector<::string>& operands){
-    
-}
-//Execute SUB instruction
-void executeSUB(vector<::string>& operands){
-    //Assuming operands are in the form {"ADD","xd","x1","x2"}
-    uint64_t results = getRegisterValue(operands[2])//Retrieves the value of the register specifed by the second operand("Xn")
-     - getRegisterValue(operands[3]);//Retrieves the value of the register specified by the third operand ("Xm")
-    setRegisterValue(operands[1],result);//Stores the result of the AND operation in the register specified by the first operand ("xd")  
-    
-}
-//Execute STR instruction
-void executeSTR(vector<::string>& operands){
-    
-}
-//Execute STRB instruction
-void executeSTRB(vector<::string>& operands){
-    
-}
+
+    //Execute NOP instruction 
+void executeNOP(vector<::string>&operands) {
+        //No operation, do nothing
+    }
+    //Execute RET instruction 
+void executeRET(vector<::string>&operands) {
+        cout<<"Emulation complete: Program finished\n"
+    }
+    //Execute SUB instruction 
+void executeSUB(string& op1, string& op2, string& op3) {
+        //Assuming operands are in the form {"ADD","xd","x1","x2"}
+        register[op1] = register[op2] - register[op3];
+    }
+    //Execute STR instruction
+void executeSTR(string& op1, string& op2) {
+    int address = register[op2]; // int address is assigned the value from op 2 is the register containing the address
+    memory[address] = register[op1]; //this assigns value in op1 to the memeory address
+    }
+    //Execute STRB instruction
+void executeSTRB(string& op1, string& baseReg, int regOffset) {
+    int address = register[baseReg] + regOffset; //Takes the base registers mem address  and adds the offset to calculate the new mem address
+    memory[address] = register[op1] & 0x0f; //the stores the lower 8 bits from the source register into memory
+    }
