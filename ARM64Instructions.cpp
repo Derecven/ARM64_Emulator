78aee9f5948ec407b06e1b2443ea52279a5dcdb3
JluceroB <93612514+JluceroB@users.noreply.github.com>
2023-10-09 11:37:16 -0400

    Update ARM64Instructions.cpp

--- ARM64Instructions.cpp
+++ ARM64Instructions.cpp
@@ -11,52 +11,51 @@ int memory[1000] = { 0 };// simple temp mem array
 
 //Execute AND instruction 
 void executeAND(string op1, string op2, string op3) {
-    x_registers[op1] = x_registers[op2] & x_registers[op3];
+    x_registers[op1] = x_registers[op2] & x_registers[op3];//Takes the value inside register op2 and op3 and performs bitwise operation, stores resutls in op1.
 }
 //Execute ADD instruction
 void executeADD(string op1, string op2, string op3) {
     //Assuming operands are in the form {"ADD","xd","x1","x2"}
-    x_registers[op1] = x_registers[op2] + x_registers[op3];
+    x_registers[op1] = x_registers[op2] + x_registers[op3];//Takes the value inside register op2 and op3 and add them together, stores resutls in op1.
 }
 //Execute B instruction 
 void executeB(string destination) {
     //Assuming operands are in the form {"B","target_address"}
-    cout << "Branching to: " << destination << "\n";
-    pc_register = destination;//update program counter to the target address
+    cout << "Branching to: " << destination << "\n";//branches to destination provided by instruction
+    pc_register = destination;//updates program counter to the new destination
 }
-//Execute B.GET instruction (branch is greater or equal to) 
+//Execute B.GET instruction (branch if greater or equal to) 
 void executeBGET(string destination, string op1, string op2) {
     //Assuming operands are in the form {"B","target_address"}
     int operand1 = x_registers[op1]; //storing value from register op1
-    int operand2 = stoi(op2); //assumes op2 is the immidiate value converts to integer
+    int operand2 = stoi(op2); //assumes op2 is the immidiate value converts string to integer.
     
-    //checks conditon which is if it is greater than
+    //checks conditon which is if it is greater than or equal
     if (operand1 >= operand2) {
         cout << "B.GET: Branch to: " << destination << "\n";
-        pc_register = destination;
+        pc_register = destination;//updates  program counter to new destination
     }
     else {
-        cout << "B.GET: Cannot branch condition has not been met.\n";
+        cout << "B.GET: Cannot branch condition has not been met.\n";//if branch instruction is not greater or equal too, error message is printed out.
     }
 }
 //Execute B.LE instruction(less then or equal to)
 void executeBLE(string destination, string op1, string op2) {
     //Assuming operands are in the form {"B","target_address"}
     int operand1 = x_registers[op1]; //storing value from register op1
-    int operand2 = atoi(op2); //assumes op2 is the immidiate value converts to integer
+    int operand2 = atoi(op2); //assumes op2 is the immidiate value converts string to integer
 
-    //checks conditon which is if it is greater than
+    //checks conditon which is if it is less than or equal to
     if (operand1 <= operand2) {
-        cout << "B.GET: Branch to: " << destination << "\n";
-        pc_register = destination;
+        cout << "B.GET: Branch to: " << destination << "\n";//branch to destination
+        pc_register = destination;//update program counter to destination.
     }
     else {
-        cout << "B.GET: Cannot branch condition has not been met.\n";
+        cout << "B.GET: Cannot branch condition has not been met.\n";//If condition is not met, error message is printed out.
     }
 }
-//Execute CMP instruction 
+//Execute CMP instruction (compare)
 void executeCMP(string op1, string op2) {
-    //Assuming operands are in the form {"B","target_address"}
     int zFlag = 0;
     int nFlag = 0;
     int operand1 = x_registers[op1]; //storing value from register op1
@@ -66,37 +65,36 @@ void executeCMP(string op1, string op2) {
     int result = operand1 - operand2;
     //set N(negative) and Z(zero) flags
     if (result == 0) {
-        zFlag++;
+        zFlag++;//increment Z flag is results from compare is 0
     }
     else if (result < 0) {
-        nFlag++;
+        nFlag++;//If result is less than zero increment N flag
     }
-    cout << "CMP: Zerp Flag = " << zFlag << "Negative Flag = " << nFlag << "\n";
+    cout << "CMP: Zerp Flag = " << zFlag << "Negative Flag = " << nFlag << "\n";//print out result of both flags
 }
 //Execute EOR instruction
 void executeEOR(string op1, string op2, string op3) {
-    register[op1] = x_registers[op1] ^ x_registers[op2]; // performs bitwise XOR
+    register[op1] = x_registers[op1] ^ x_registers[op2]; //Takes value from op2 & op3, performs XOR bitwise op and stores results in op1.
 
 }
 //Execute LDR instruction
 void executeLDR(string op1, string op2) {
     int address = x_registers[op2];//storing the value stored in op2 inside of address
-    register[op1] = memory[address]; //assiging that memory address to the specified target register
+    register[op1] = memory[address]; //assiging that memory address to the specified target register which is op1.
 }
 //Execute LDRB instruction (loads a byte from memory into a register)
 void executeLDRB(string op1, string baseReg, int regOffset) {
     int address = x_registers[baseReg] + regOffset; //Takes the base registers mem address  and adds the offset to calculate the new mem address
-    register[op1] = memory[address]; //the byte is loaded from mem to the location of destination address
+    register[op1] = memory[address]; //the byte is loaded from mem address to the location of new destination.
 }
 //Execute MOV instruction
 void executeMOV(string destination, string op1) {
     int operand1 = x_registers[op1]; //storing the value store in op1 inside of operand 1
-    register[destination] = operand1; //moving the value of operand 1 into the destination register
+    register[destination] = operand1; //moving the value of operand 1 into the destination register.
 }
 //Execute MUL instruction 
 void executeMUL(string op1, string op2, string op3) {
-
-    register[op1] = x_registers[op2] * x_registers[op3]
+    register[op1] = x_registers[op2] * x_registers[op3]; //Takes values from op2 and op3, multiplies them and stores results in register op1.
 }
 
     //Execute NOP instruction 
@@ -105,17 +103,16 @@ void executeNOP(string operands) {
     }
     //Execute RET instruction 
 void executeRET(string operand) {
-    cout << "Emulation complete: Program finished\n";
+    cout << "Emulation complete: Program finished\n";//Prints program has finished statement.
     }
     //Execute SUB instruction 
 void executeSUB(string op1, string op2, string op3) {
-        //Assuming operands are in the form {"ADD","xd","x1","x2"}
-        register[op1] = x_registers[op2] - x_registers[op3];
+        register[op1] = x_registers[op2] - x_registers[op3];//Takes value from op2 and op3 and stores the value in op1.
     }
     //Execute STR instruction
 void executeSTR(string op1, string op2) {
-    int address = x_registers[op2]; // int address is assigned the value from op 2 is the register containing the address
-    memory[address] = x_registers[op1]; //this assigns value in op1 to the memeory address
+    int address = x_registers[op2]; // int address is assigned the value from op 2 
+    memory[address] = x_registers[op1]; //this assigns value in op2 to the memeory address
     }
     //Execute STRB instruction
 void executeSTRB(string op1, string baseReg, int regOffset) {
